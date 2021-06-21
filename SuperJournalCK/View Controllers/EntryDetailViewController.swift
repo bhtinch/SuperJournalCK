e7d20906cd472d3380730ad9ffbf818272e7eb2e
Benjamin Tincher <b.h.tincher@gmail.com>
2021-06-21 12:56:32 -0600

    storyboards laid out

---
+++ SuperJournalCK/View Controllers/EntryDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EntryDetailViewController.swift
+//  SuperJournalCK
+//
+//  Created by Benjamin Tincher on 6/21/21.
+//
+
+import UIKit
+
+class EntryDetailViewController: UIViewController {
+    //  MARK: - OUTLETS
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var bodyTextView: UITextView!
+    
+    //  MARK: - PROPERTIES
+
+    //  MARK: - LIFECYCLES
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //  MARK: - ACTIONS
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    //  MARK: - METHODS
+
+}
---
+++ SuperJournalCK/View Controllers/JournalListViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ConverstationListViewController.swift
+//  SuperJournalCK
+//
+//  Created by Benjamin Tincher on 6/21/21.
+//
+
+import UIKit
+
+class JournalListViewController: UIViewController {
+    //  MARK: - OUTLETS
+    @IBOutlet weak var tableView: UITableView!
+    
+    //  MARK: - PROPERTIES
+
+    //  MARK: - LIFECYCLES
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    //  MARK: - ACTIONS
+    
+    //  MARK: - METHODS
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}   //  End of Class
+
+extension JournalListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}   //  End of Extension
